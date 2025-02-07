dd8ba9e8ae9217d05ff33967e0939d86c31c6829
judonwang <judonwang@gmail.com>
2025-02-07 15:18:51 -0800

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -63,6 +63,7 @@ from ultralytics.nn.modules import (
     TorchVision,
     WorldDetect,
     v10Detect,
+    Involution,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
@@ -981,6 +982,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             PSA,
             SCDown,
             C2fCIB,
+            Involution,
         }
     )
     repeat_modules = frozenset(  # modules with 'repeat' arguments
