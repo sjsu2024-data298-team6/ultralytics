3d7be29644652c86c99ab991dc615756b9a61099
judonwang <judonwang@gmail.com>
2025-02-17 18:31:48 -0800

    trying invo isntead of conv

--- ultralytics/nn/modules/transformer.py
+++ ultralytics/nn/modules/transformer.py
@@ -429,58 +429,59 @@ class DeformableTransformerDecoder(nn.Module):
     
 
 # CMHSA
-#class CMHSA(nn.Module):
-#    """
-#    Implementing convolutional multihead self attention
-#    as is from https://doi.org/10.1109/TIM.2023.3241825
-#    """
-#    def __init__(self, embed_dim, num_heads):
-#        super().__init__()
+# class CMHSA(nn.Module):
+#     """
+#     Implementing convolutional multihead self attention
+#     as is from https://doi.org/10.1109/TIM.2023.3241825
+#     """
+#     def __init__(self, embed_dim, num_heads):
+#         super().__init__()
 
-        # used for the scaling for Q @ K
-#        self.head_dim = embed_dim/num_heads
+#         # used for the scaling for Q @ K
+#         self.head_dim = embed_dim/num_heads
 
-        # One key difference is that the paper uses convolutions instead of linear for the vectors
-#        self.q = nn.Conv2d(embed_dim, embed_dim, kernel_size=1, bias=False)
-#        self.k = nn.Conv2d(embed_dim, embed_dim, kernel_size=1, bias=False)
-#        self.v = nn.Conv2d(embed_dim, embed_dim, kernel_size=1, bias=False)
+#         # One key difference is that the paper uses convolutions instead of linear for the vectors
+#         self.q = nn.Conv2d(embed_dim, embed_dim, kernel_size=1, bias=False)
+#         self.k = nn.Conv2d(embed_dim, embed_dim, kernel_size=1, bias=False)
+#         self.v = nn.Conv2d(embed_dim, embed_dim, kernel_size=1, bias=False)
 
-        # Paper has a conv layer between scaled dot product and softmax
-#        self.conv1 = nn.Conv2d(embed_dim, embed_dim, kernel_size=1, bias=False)
+#         # Paper has a conv layer between scaled dot product and softmax
+#         self.conv1 = nn.Conv2d(embed_dim, embed_dim, kernel_size=1, bias=False)
 
-#        self.sm = nn.Softmax(dim=-1)
-#        self.norm = nn.InstanceNorm2d(embed_dim)
-#        self.fc1 = nn.Linear(embed_dim, embed_dim, bias=False)
+#         self.sm = nn.Softmax(dim=-1)
+#         self.norm = nn.InstanceNorm2d(embed_dim)
+#         self.fc1 = nn.Linear(embed_dim, embed_dim, bias=False)
 
-#    def forward(self, x):
-#        b, c, h, w = x.shape
+#     def forward(self, x):
+#         b, c, h, w = x.shape
 
-#        q = self.q(x)
-#        k = self.k(x)
-#        v = self.v(x)
+#         q = self.q(x)
+#         k = self.k(x)
+#         v = self.v(x)
 
-        # Flatten layer for subsequent processing; this needs to be done for the transformer to use convs
-#        q = q.flatten(2).permute(0, 2, 1) # Put into form (b, h*w, c)
-#        k = k.flatten(2).permute(0,2,1)
-#        v = v.flatten(2).permute(0,2,1)
+#         # Flatten layer for subsequent processing; this needs to be done for the transformer to use convs
+#         q = q.flatten(2).permute(0, 2, 1) # Put into form (b, h*w, c)
+#         k = k.flatten(2).permute(0,2,1)
+#         v = v.flatten(2).permute(0,2,1)
 
-        # scaled dot product
-#        attn_score = torch.matmul(q, k.t()) / (self.head_dim**0.5)
+#         # scaled dot product
+#         # transpose with (1,2) to allow matrix multiplication
+#         attn_score = torch.matmul(q, k.transpose(1,2)) / (self.head_dim**0.5)
 
-        # conv
-#        attn_score = self.conv1(attn_score)
+#         # conv
+#         attn_score = self.conv1(attn_score)
 
-        # paper uses softmax, then uses instance normalization
-#        attn_score = self.sm(attn_score)
-#        attn_score = self.norm(attn_score)
+#         # paper uses softmax, then uses instance normalization
+#         attn_score = self.sm(attn_score)
+#         attn_score = self.norm(attn_score)
 
-        # dot product QK and V
-#        output = torch.matmul(attn_score, v)
-#        output = self.fc1(output)
+#         # dot product QK and V
+#         output = torch.matmul(attn_score, v)
+#         output = self.fc1(output)
 
-        # paper uses a view to reshape output
-#        output = output.permute(0, 2, 1).reshape(b, c, h, w) # reshape to proper shape
-#        return output + x
+#         # paper uses a view to reshape output
+#         output = output.permute(0, 2, 1).reshape(b, c, h, w) # reshape to proper shape
+#         return output + x
     
 #class ECTB(nn.Module):
 #    """
