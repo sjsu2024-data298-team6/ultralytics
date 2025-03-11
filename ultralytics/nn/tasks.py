3a24c2d330831d63e01a055e05b5fc51a3d0a8ef
Sungwon Lee <82964002+sgwlee96@users.noreply.github.com>
2025-03-10 19:15:46 -0700

    Update tasks.py
    Signed-off-by: Sungwon Lee <82964002+sgwlee96@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -64,6 +64,7 @@ from ultralytics.nn.modules import (
     v10Detect,
     BiFPNLayer, RTDETRDecoderCustom, MultiDetect,
     MHSA,
+    BiFPN_Concat3,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
