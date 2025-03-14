0ff6b07f538e702d15df5948069180e619b6970b
Sungwon Lee <82964002+sgwlee96@users.noreply.github.com>
2025-03-14 16:21:11 -0700

    Update conv.py
    Signed-off-by: Sungwon Lee <82964002+sgwlee96@users.noreply.github.com>

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -23,6 +23,7 @@ __all__ = (
     "RepConv",
     "Index",
     "BiFPN_Concat3",
+    "BiFPN_Concat2",
 )
 
 
@@ -353,7 +354,18 @@ class Index(nn.Module):
 
 
 #################################################################################################
+class BiFPN_Concat2(nn.Module):
+    def __init__(self, dimension=1):
+        super(BiFPN_Concat2, self).__init__()
+        self.d = dimension
+        self.w = nn.Parameter(torch.ones(2, dtype=torch.float32), requires_grad=True)
+        self.epsilon = 0.0001
 
+    def forward(self, x):
+        w = self.w
+        weight = w / (torch.sum(w, dim=0) + self.epsilon)
+        x = [weight[0] * x[0], weight[1] *x [1]]
+        return torch.cat(x, self.d)
 
 class BiFPN_Concat3(nn.Module):
     def __init__(self, dimension=1):
