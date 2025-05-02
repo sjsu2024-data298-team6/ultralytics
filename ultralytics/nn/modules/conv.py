9a65170f2c942d102f33d4afc74a68591c405c44
Sungwon Lee <82964002+sgwlee96@users.noreply.github.com>
2025-05-02 14:31:46 -0700

    Update conv.py
    Signed-off-by: Sungwon Lee <82964002+sgwlee96@users.noreply.github.com>

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -362,12 +362,21 @@ class BiFPN_Concat2(nn.Module):
         self.d = dimension
         self.w = nn.Parameter(torch.ones(2, dtype=torch.float32), requires_grad=True)
         self.epsilon = 0.0001
-
+        
     def forward(self, x):
-        w = self.w
-        weight = w / (torch.sum(w, dim=0) + self.epsilon)
-        x = [weight[0] * x[0], weight[1] *x [1]]
-        return torch.cat(x, self.d)
+        weight = self.w / (torch.sum(self.w) + self.epsilon)
+        target_size = min([xi.shape[2:] for xi in x], key=lambda s: s[0] * s[1])
+        x_resized = [
+            F.interpolate(xi, size=target_size, mode='nearest') if xi.shape[2:] != target_size else xi
+            for xi in x
+        ]
+        x_weighted = [weight[i] * x_resized[i] for i in range(2)]
+        return torch.cat(x_weighted, dim=self.d)
+    # def forward(self, x):
+    #     w = self.w
+    #     weight = w / (torch.sum(w, dim=0) + self.epsilon)
+    #     x = [weight[0] * x[0], weight[1] *x [1]]
+    #     return torch.cat(x, self.d)
 
 class BiFPN_Concat3(nn.Module):
     def __init__(self, dimension=1):
